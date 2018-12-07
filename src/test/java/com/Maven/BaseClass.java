176e7706738728256c26750984a64ff1d11e223c
Kilanya <Kilanya456@yahoo.com>
2018-12-06 21:55:54 -0500

    initial commit

---
+++ src/test/java/com/Maven/BaseClass.java
@@ -0,0 +1,27 @@
+package com.Maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.Test;
+
+		public class BaseClass {
+	    
+	    @Test
+	    public void setup() {
+	    	
+	        //System.setProperty(“webdriver.chrome.driver”, “/Users/waqaskhan/Documents/JAR Files/chromedriver”);	    	
+	        WebDriver  driver = new HtmlUnitDriver();
+	        
+//	        driver.manage().window().fullscreen();
+	        
+	        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	        
+	        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	        
+	        driver.get("https://opensource-demo.orangehrmlive.com/");
+	        	
+	    }
+
+	}
