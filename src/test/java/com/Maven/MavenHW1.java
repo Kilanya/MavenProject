e2ad44f8b58e19c971c6b49c0cb20904e92189b8
Kilanya <Kilanya456@yahoo.com>
2018-12-08 12:56:35 -0500

    Made more changes

--- src/test/java/com/Maven/MavenHW1.java
+++ src/test/java/com/Maven/MavenHW1.java
@@ -12,7 +12,7 @@ import org.testng.Assert;
 	          WebDriver driver = new ChromeDriver();
 	          String url="https://www.google.com/";
 	          driver.get(url);
-	          String title  =driver.getTitle();
+	          String title = driver.getTitle();
 
 	          Assert.assertEquals("Google", title);
 	          
